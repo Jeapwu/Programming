aeff5bc7974e1e55e23986a137699539c47f3f51
jack <jeapwu@163.com>
2020-11-29 07:23:39 -0500

    2020-11-29

--- cpp/concurrency/condition variable/condvar2.cpp
+++ cpp/concurrency/condition variable/condvar2.cpp
@@ -10,17 +10,25 @@ std::queue<int> queue;
 std::mutex queueMutex;
 std::condition_variable queueCondvar;
 
+//生产者
 void provider(int var){
     for(int i=0;i<6;i++){
         {
             std::lock_guard<std::mutex> lg(queueMutex);
             queue.push(var+i);
         }
+        /**
+         * 每次quque中加入新元素都会唤醒一个线程输出该元素.还有注意上面这条并不在保护区内,
+         * 也不需要在保护区内.虽然std::queue是的读写操作不能同时进行,
+         * 但是当保护区结束的时候push()操作已经完成了,
+         * 而对std::queue执行读操作在另外线程的时候也是被上锁的因此不可能会产生data race.
+         */
         queueCondvar.notify_one();
         std::this_thread::sleep_for(std::chrono::milliseconds(var));
     }  
 }
 
+//消费者
 void consumer(int num){
     while(true){
         int val;
