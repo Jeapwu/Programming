540d1702f54b13b04b1c6e7a76e662c09120aefe
Jeapwu <jeapwu@163.com>
2020-11-30 11:10:45 +0800

    2020-11-30

---
+++ cpp/internationalization/facet/timeput.cpp
@@ -0,0 +1,18 @@
+#include <locale>
+#include <chrono>
+#include <ctime>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    auto now=std::chrono::system_clock::now();
+    time_t t=std::chrono::system_clock::to_time_t(now);
+    tm* nowTM=std::localtime(&t);
+
+    locale locC;
+    const time_put<char>& p=use_facet<time_put<char>>(locC);
+
+    string format="%A %x\n";
+    p.put(cout,cout,':',nowTM,format.c_str(),format.c_str()+format.size());
+}
